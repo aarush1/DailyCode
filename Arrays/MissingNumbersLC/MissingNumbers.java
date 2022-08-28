46bfab77c05b740f727af024eefd951a85387ff0
Aarush Verma <aarushverma@Aarushs-MacBook-Pro.local>
2022-08-28 00:40:39 -0500

    initial dump

---
+++ Arrays/MissingNumbersLC/MissingNumbers.java
@@ -0,0 +1,7 @@
+package MissingNumbersLC;
+
+public class MissingNumbers {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ StriverA-Z-DSA/Basic/IfElse.java
@@ -0,0 +1,14 @@
+package Basic;
+
+public class IfElse {
+    static String compareNM(int n, int m) {
+        if (n < m) {
+            return "lesser";
+        } else if (n > m) {
+            return "greater";
+        } else {
+            return "equal";
+        }
+    }
+}
+
