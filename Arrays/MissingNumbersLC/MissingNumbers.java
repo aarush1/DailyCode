471a157f6ee0c68aa11743ce6a1e8da496644a9e
Aarush Verma <aarushverma@Aarushs-MacBook-Pro.local>
2022-08-29 17:06:16 -0500

    Day 1

--- Arrays/MissingNumbersLC/MissingNumbers.java
+++ Arrays/MissingNumbersLC/MissingNumbers.java
@@ -1,7 +1,20 @@
 package MissingNumbersLC;
 
 public class MissingNumbers {
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(int[] nums) {
+        int sum = 0;
+
+        int n = nums.length;
+
+        int rangesum = n*(n+1)/2;
+
+        for(int i = 0; i<n;i++){
+            sum+= nums[i];
+        }
+
+        return rangesum - sum;
+
     }
-}
+
+    }
+
